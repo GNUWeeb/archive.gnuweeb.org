ea22494095b34dbd6d7139a6110e7b479b850789
Ammar Faizi <ammarfaizi2@gmail.com>
2021-07-28 21:48:24 +0700

    Add adminer and telegram page
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

---
+++ public/telegram/index.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GNU/Weeb Telegram Chat Archive</title>
+	<link rel="stylesheet" type="text/css" href="/assets/css/telegram.css"/>
+	<script type="text/javascript" src="/assets/js/telegram.js"></script>
+</head>
+<body>
+	<div class="main-cg">
+		<h1>GNU/Weeb Telegram Chat Archive</h1>
+		<div id="chat-cg">
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+			<div class="msg-cg">
+				<div class="pp-cg"></div>
+				<div class="content-cg"></div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
