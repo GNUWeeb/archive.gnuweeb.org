GNUWeeb/archive.gnuweeb.org

commit b5cdc0a043a899acb11189f4e626586aa76cef58

telegram/index: Add external online API support
I don't want to put too much burden for the frontend team. This commit
adds external API support. This means frontend developer can just use
our online API without doing full complicated backend setup.

Setup instructions:
  git clone https://github.com/GNUWeeb/archive.gnuweeb.org;
  cd archive.gnuweeb.org;
  export API_BASE_URL="https://archive.gnuweeb.org";
  php -S 0.0.0.0:8005 -t public;

  # Open http://127.0.0.1:8005/telegram on browser

Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

public/telegram/index.php
@@ -20,6 +20,16 @@
 	<div id="chat-cg">
 	</div>
 </div>
+<script type="text/javascript">
+const API_BASE_URL = <?php
+$apiBaseUrl = getenv("API_BASE_URL");
+if ($apiBaseUrl) {
+	echo json_encode($apiBaseUrl, JSON_UNESCAPED_SLASHES);
+} else {
+	echo json_encode("/", JSON_UNESCAPED_SLASHES);
+}
+?>;
+</script>
 <script type="text/javascript" src="/assets/js/telegram.js?w=<?= time() ?>"></script>
 </body>
 </html>

public/assets/css/telegram.css
@@ -8,8 +8,8 @@ body {
 #chat-cg {
 	margin: auto;
 	border: 1px solid #000;
-	width: 1250px;
-	height: 700px;
+	width: 90%;
+	height: 500px;
 	overflow-y: scroll;
 }
 .msg-cg {

public/assets/js/telegram.js
@@ -28,7 +28,7 @@ function fetch_msg(group_id, start_at, limit, callback, tg_date_sort = "asc")
 			throw e;
 		}
 	};
-	ch.open("GET", "/telegram/api.php?group_id="+group_id+"&start_at="+
+	ch.open("GET", API_BASE_URL+"/telegram/api.php?group_id="+group_id+"&start_at="+
 		start_at+"&limit="+limit+"&tg_date_sort="+tg_date_sort);
 	ch.send();
 }

public/telegram/api.php
@@ -122,9 +122,7 @@ class messageContext
 		$fields = implode(",", self::dumpMessagesFields());
 		$data[] = $this->chatId;
 		$query  = <<<SQL
-			SELECT 
-				{$fields}
-			FROM gw_group_messages AS a
+			SELECT {$fields} FROM gw_group_messages AS a
 			INNER JOIN gw_groups AS b ON b.id = a.group_id
 			INNER JOIN gw_group_message_data AS c ON a.id = c.msg_id
 			LEFT JOIN gw_files AS d ON d.id = c.file
@@ -135,7 +133,10 @@ class messageContext
 			$query .= " AND c.msg_id <= ? ";
 		}
 		$query .= " ORDER BY c.tg_date DESC LIMIT {$this->limit}";
-		$query = "SELECT * FROM ({$query}) x ORDER BY tg_date {$tgDateSort};";
+
+		if (strtolower($tgDateSort) !== "desc")
+			$query = "SELECT * FROM ({$query}) x ORDER BY tg_date ASC;";
+
 		return $this->pdo->prepare($query);
 	}
 

commit 023f5bcaf96c4cd9d21fb1646a5e56fe78c2f940

telegram: When API_BASE_URL is empty, don't set base URL
When we have local backend, don't override the base URL.

Fixes: b5cdc0a043a899acb11189f4e626586aa76cef58 ("telegram/index: Add external online API support")
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

public/telegram/index.php
@@ -27,7 +27,7 @@ $apiBaseUrl = getenv("API_BASE_URL");
 if ($apiBaseUrl) {
 	echo json_encode($apiBaseUrl, JSON_UNESCAPED_SLASHES);
 } else {
-	echo json_encode("/", JSON_UNESCAPED_SLASHES);
+	echo "\"\"";
 }
 ?>;
 </script>